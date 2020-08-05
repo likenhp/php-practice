8fdab433504621f4d4a0f0b3992973cd8b59b223
likenhp <likenhp@gmail.com>
2020-08-05 15:21:16 -0700

    understanding interfaces

--- interfaces.php
+++ interfaces.php
@@ -1,27 +1,73 @@
 <?php
 
 // interfaces are blueprints to group together different classes
+// classes must have the same methods as the interface to be part of the interface
+interface PaymentInterface {
+  // the functions must be the same as the classes
+  public function payNow();
+}
 
-class Paypal {
+interface LoginInterface {
+  public function loginFirst();
+}
+
+class Paypal implements PaymentInterface, LoginInterface {
+  public function loginFirst() {
+  }
+
   public function payNow() {
+  }
 
+  public function paymentProcess() {
+    $this->loginFirst();
+    $this->payNow();
   }
 }
 
-class Visa {
+class BankTransfer implements PaymentInterface, LoginInterface {
+  public function loginFirst() {
+  }
+
   public function payNow() {
-    
+  }
+
+  public function paymentProcess() {
+    $this->loginFirst();
+    $this->payNow();
   }
 }
 
-class Cash {
+class Visa implements PaymentInterface {
   public function payNow() {
-    
+  }
+
+  public function paymentProcess() {
+    $this->payNow();
+  }
+}
+
+class Cash implements PaymentInterface {
+  public function payNow() {
+  }
+
+  public function paymentProcess() {
+    $this->payNow();
   }
 }
 
 class BuyProduct {
-  public function pay($payType) {
+  public function pay(PaymentInterface $payType) {
+    // i can use the methods within interface inside of this object
+    $payType->paymentProcess();
+  }
 
+  public function payOnline(LoginInterface $payType) {
+    // i can use the methods within interface inside of this object
+    $payType->paymentProcess();
   }
 }
+
+// $payType = new Paypal();
+$payType = new Cash();
+$buyProduct = new BuyProduct();
+$buyProduct->pay($payType); 
